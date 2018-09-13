02c422b93b44652a455a4817cc1446bee1044a5f
Jason Pitts <juniorjr083@gmail.com>
2018-09-13 01:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Code your solution in this file!
 
-function distance
+function distanceFromHqInBlocks(block){
+   if(block > 42 ){
+     return block - 42;
+     
+   }
+   else{
+     return 42 - block;
+   }
+  
+}
